d03d5b47b5ac8fe041193242b6d480e468dba156
Shruti Kumari <shrutikumari0811@gmail.com>
2025-01-29 09:44:35 +0000

    image issue solved after deployment

--- app/qrmapping_dashboard/dist/Component-dbg.js
+++ app/qrmapping_dashboard/dist/Component-dbg.js
@@ -1,7 +1,33 @@
+// sap.ui.define([
+//     "sap/ui/core/UIComponent",
+//     "com/ingenx/qrmappingdashboard/model/models"
+// ], (UIComponent, models) => {
+//     "use strict";
+
+//     return UIComponent.extend("com.ingenx.qrmappingdashboard.Component", {
+//         metadata: {
+//             manifest: "json",
+//             interfaces: [
+//                 "sap.ui.core.IAsyncContentCreation"
+//             ]
+//         },
+
+//         init() {
+//             UIComponent.prototype.init.apply(this, arguments);
+
+//             this.setModel(models.createDeviceModel(), "device");
+
+//             this.getRouter().initialize();
+//         }
+//     });
+// });
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "com/ingenx/qrmappingdashboard/model/models"
-], (UIComponent, models) => {
+    "com/ingenx/qrmappingdashboard/model/models",
+    "sap/base/Log",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
+], (UIComponent, models, Log, JSONModel, Device) => {
     "use strict";
 
     return UIComponent.extend("com.ingenx.qrmappingdashboard.Component", {
@@ -13,13 +39,14 @@ sap.ui.define([
         },
 
         init() {
-            // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
             this.setModel(models.createDeviceModel(), "device");
 
-            // enable routing
+            const imagePath = sap.ui.require.toUrl("com/ingenx/qrmappingdashboard/images/");
+            const imageModel = new JSONModel({ path: imagePath });
+            this.setModel(imageModel, "imageModel");
+
             this.getRouter().initialize();
         }
     });
--- app/qrmapping_dashboard/webapp/Component.js
+++ app/qrmapping_dashboard/webapp/Component.js
@@ -1,7 +1,33 @@
+// sap.ui.define([
+//     "sap/ui/core/UIComponent",
+//     "com/ingenx/qrmappingdashboard/model/models"
+// ], (UIComponent, models) => {
+//     "use strict";
+
+//     return UIComponent.extend("com.ingenx.qrmappingdashboard.Component", {
+//         metadata: {
+//             manifest: "json",
+//             interfaces: [
+//                 "sap.ui.core.IAsyncContentCreation"
+//             ]
+//         },
+
+//         init() {
+//             UIComponent.prototype.init.apply(this, arguments);
+
+//             this.setModel(models.createDeviceModel(), "device");
+
+//             this.getRouter().initialize();
+//         }
+//     });
+// });
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "com/ingenx/qrmappingdashboard/model/models"
-], (UIComponent, models) => {
+    "com/ingenx/qrmappingdashboard/model/models",
+    "sap/base/Log",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
+], (UIComponent, models, Log, JSONModel, Device) => {
     "use strict";
 
     return UIComponent.extend("com.ingenx.qrmappingdashboard.Component", {
@@ -16,6 +42,10 @@ sap.ui.define([
             UIComponent.prototype.init.apply(this, arguments);
 
             this.setModel(models.createDeviceModel(), "device");
+
+            const imagePath = sap.ui.require.toUrl("com/ingenx/qrmappingdashboard/images/");
+            const imageModel = new JSONModel({ path: imagePath });
+            this.setModel(imageModel, "imageModel");
 
             this.getRouter().initialize();
         }
